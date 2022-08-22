5065830180bdfc3b251c4ba2e879122f86233a7a
木下拓哉 <47770062+TakuyaKinoshita@users.noreply.github.com>
2022-08-22 21:58:29 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,20 +1,17 @@
-from discord.ext import commands
 import os
-import traceback
-
-bot = commands.Bot(command_prefix='/')
-
-
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+import discord
 
 token = os.environ['DISCORD_BOT_TOKEN']
-bot.run(token)
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message):
+        print(f'Message from {message.author}: {message.content}')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+client.run(token)
